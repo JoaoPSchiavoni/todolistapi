ec517c347db2524e2108dc4eaf181b3858593ed1
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2026-01-07 07:25:09 -0300

    Configuracao inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from todolist_api.app import app
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Ola mundo!1234'}
+    assert response.status_code == HTTPStatus.OK
